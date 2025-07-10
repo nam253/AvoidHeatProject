966365ccff608e371ed1ff0f2284c9cdefd6d839
morningB <yjw001205@gmail.com>
2025-07-10 16:06:19 +0900

    feat: 사람 체온 조절 추가

---
+++ Assets/JW/Scripts/HumanTemperature.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class HumanTemperature : LivingEntity
+{
+    public AudioClip itemPickupClip; // 아이템 습득 소리
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        // 아이템과 충돌한 경우 해당 아이템을 사용하는 처리
+        if (!dead)
+        {
+            IItem item = other.GetComponent<IItem>();
+            if (item != null)
+            {
+                item.Use(gameObject);
+                Debug.Log(humanTemperature);
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
